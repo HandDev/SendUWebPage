d676ea9c72424ed119f12d1070301b727badb8dd
ParkJaesung <jspark141515@gmail.com>
2016-10-17 21:07:47 +0900

    Created new project with webpage
    Created new project with webpage

---
+++ app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by parkjaesung on 2016. 10. 17..
+ */
+
+var express = require('express');
+
+var app = express();
+app.set('view engine', 'html');
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', function(req, res){
+   res.render('index')
+});
+
+var server = app.listen(3000, function(){
+    console.log("Express server has started on port 3000")
+})
+
