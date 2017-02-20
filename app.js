1a513019b5fc053a8fd36f4208dfad43c4e39fa2
ParkJaesung <jspark141515@gmail.com>
2017-02-21 02:35:47 +0900

    kjh

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 app.set('view engine', 'html');
 app.use(express.static(__dirname + '/views'));
 
-app.use('/assets',express.static(path.join(__dirname, 'public')));
+app.use('/assets',express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req, res){
    res.render('index')
